db39f1a835a33d1b44c06ce03b068d5331ff17b5
monsur776e <monsuralam8897@gmail.com>
2023-05-19 00:15:48 +0600

    route setup done

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Shared/Navbar/Navbar";
+import Footer from "./Shared/Footer/Footer";
+
+const Home = () => {
+   return (
+      <>
+         <Navbar></Navbar>
+         <Footer></Footer>
+      </>
+   );
+};
+
+export default Home;
